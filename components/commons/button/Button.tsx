92cdd8f70fb9a6791abea59097d5d0c903b36d08
pol <pol.armaza.002@gmail.com>
2023-01-02 11:08:21 -0500

    update buttons / link menu web / update img module1

--- components/commons/button/Button.tsx
+++ components/commons/button/Button.tsx
@@ -5,7 +5,7 @@ type Props = {
     // style: string | null | undefined;
   };
 
-const Button = ({text, style}:Props) => {
+const Button = ({text}:Props) => {
   return (
     <div className={`bg-sky-500 rounded-xl py-2 px-5 min-w[5rem] lg:min-w-[10rem] text-white font-bold text-xs  lg:text-xl cursor-pointer z-20 shadow-md shadow-black  mx-auto `}>{text}</div>
   )
