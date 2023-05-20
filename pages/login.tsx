da3b04843d413d937a1ab88382c3f6a4785ed988
pol <pol.armaza.002@gmail.com>
2023-05-19 19:27:09 -0500

    vista final

--- pages/login.tsx
+++ pages/login.tsx
@@ -37,6 +37,7 @@ const Login = () => {
   } = useForm<Form>();
 
   const onSubmit: SubmitHandler<Form> = async (data) => {
+    return toast.error("ocurrió un error");
     const { email, password } = data;
     if (email === "" || password === "")
       return toast.error("Llenar todos los campos");
